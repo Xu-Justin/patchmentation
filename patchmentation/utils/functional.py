51185e825218949c0ed539b408ae277687f07936
William Justin <williamjustinxu@gmail.com>
2022-11-07 20:40:16 +0700

    remove place_image_array and visibility_suppression

--- patchmentation/utils/functional.py
+++ patchmentation/utils/functional.py
@@ -43,68 +43,6 @@ def scale_bbox(bbox: BBox, scale: float) -> BBox:
     ymax = ymin + scaled_height
     return BBox(xmin, ymin, xmax, ymax)
 
-def visibility_suppression(patches: List[Patch], visibility_threshold: float, non_removal_patches: List[Patch] = None, **kwargs) -> List[Patch]:
-    if len(patches) == 0: return []
-
-    attr_bbox = kwargs.get('attr_bbox', 'bbox')
-    attr_non_removal_patches_bbox = kwargs.get('attr_non_removal_patches_bbox', 'bbox')
-    
-    min_x, min_y, max_x, max_y = getattr(patches[0], attr_bbox)
-    for patch in patches:
-        xmin, ymin, xmax, ymax = getattr(patch, attr_bbox)
-        min_x = min(min_x, xmin)
-        min_y = min(min_y, ymin)
-        max_x = max(max_x, xmax)
-        max_y = max(max_y, ymax)
-    
-    EMPTY_CELL = -1
-    grid_width = max_x - min_x
-    grid_height = max_y - min_y
-    grid = np.full((grid_height, grid_width), EMPTY_CELL)
-    
-    total_area = [0] * len(patches)
-    visible_area = [0] * len(patches)
-
-    for i, patch in enumerate(patches):
-        xmin, ymin, xmax, ymax = getattr(patch, attr_bbox)
-        xmin -= min_x
-        xmax -= min_x
-        ymin -= min_y
-        ymax -= min_y
-        grid[ymin:ymax, xmin:xmax] = i
-        total_area[i] = getattr(patch, attr_bbox).area()
-    
-    if non_removal_patches is not None:
-        for patch in non_removal_patches:
-            xmin, ymin, xmax, ymax = getattr(patch, attr_non_removal_patches_bbox)
-            xmin -= min_x
-            xmax -= min_x
-            ymin -= min_y
-            ymax -= min_y
-            xmin = max(0, xmin)
-            ymin = max(0, ymin)
-            xmax = min(xmax, grid_width)
-            ymax = min(ymax, grid_height)
-            if xmin >= grid_width or ymin >= grid_height or xmax < 0 or ymax < 0 : continue
-            grid[ymin:ymax, xmin:xmax] = EMPTY_CELL
-    
-    for i in range(grid_height):
-        for j in range(grid_width):
-            if grid[i][j] != EMPTY_CELL:
-                visible_area[grid[i][j]] += 1
-
-    visibility = [0] * len(patches)
-    for i in range(len(patches)):
-        if total_area[i] != 0:
-            visibility[i] = visible_area[i] / total_area[i]
-
-    result_patches = []
-    for i, patch in enumerate(patches):
-        if visibility[i] > visibility_threshold:
-            result_patches.append(patch)
-    
-    return result_patches
-
 def visibility_thresholding(list_patch_bbox: List[Tuple[Patch, BBox]], visibility_threshold: float, list_non_removal_patch_bbox: List[Tuple[Patch, BBox]] = None) -> List[Tuple[Patch, BBox]]:
     if len(list_patch_bbox) == 0: return []
     
@@ -162,29 +100,6 @@ def visibility_thresholding(list_patch_bbox: List[Tuple[Patch, BBox]], visibilit
 
 def resize_image_array(image_array: np.ndarray, width: int, height: int) -> np.ndarray:
     return cv2.resize(image_array, (width, height), interpolation = cv2.INTER_AREA)
-
-def place_image_array(patch_array: np.ndarray, image_array: np.ndarray, bbox: BBox) -> BBox:
-    xmin, ymin, xmax, ymax = bbox
-    image_height, image_width, _ = image_array.shape
-    patch_height, patch_width, _ = patch_array.shape
-
-    dif_xmin = max(0, 0 - xmin)
-    dif_ymin = max(0, 0 - ymin)
-    dif_xmax = min(0, image_width - xmax)
-    dif_ymax = min(0, image_height - ymax)
-
-    patch_xmin = dif_xmin
-    patch_ymin = dif_ymin
-    patch_xmax = patch_width + dif_xmax
-    patch_ymax = patch_height + dif_ymax
-
-    image_xmin = xmin + dif_xmin
-    image_ymin = ymin + dif_ymin
-    image_xmax = xmax + dif_xmax
-    image_ymax = ymax + dif_ymax
-
-    image_array[image_ymin:image_ymax, image_xmin: image_xmax] = patch_array[patch_ymin:patch_ymax, patch_xmin:patch_xmax]
-    return BBox(image_xmin, image_ymin, image_xmax, image_ymax)
 
 def overlay_image(image_a: Image, image_b: Image, bbox: BBox) -> Image:
     assert bbox.width() == image_b.width()
