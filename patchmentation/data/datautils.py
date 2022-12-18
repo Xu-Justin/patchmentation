fdd68bbdec329e515451d83065e11f4e75f8b481
William Justin <william.justin@gdn-commerce.com>
2022-12-18 18:00:10 +0700

    fix rm and wget on windows

--- patchmentation/data/datautils.py
+++ patchmentation/data/datautils.py
@@ -1,12 +1,25 @@
-import os, shutil, tarfile
+import os, shutil, tarfile, requests
 from appdirs import user_cache_dir
+from tqdm import tqdm
 
 FOLDER_CACHE_NAME = 'patchmentation-data'
 FOLDER_CACHE = user_cache_dir(FOLDER_CACHE_NAME)
 
 def rm(path: str):
     print(f'remove {path}')
+    if os.name == 'posix':
+        _rm(path)
+    else:
+        _rmV2(path)
+
+def _rm(path: str):
     os.system(f'rm -rf {path}')
+
+def _rmV2(path: str):
+    if os.path.isdir(path) and not os.path.islink(path):
+        shutil.rmtree(path)
+    elif os.path.exists(path):
+        os.remove(path)
 
 def validate_not_exists(path: str, overwrite: bool) -> None:
     if os.path.exists(path) and overwrite:
@@ -17,8 +30,21 @@ def validate_not_exists(path: str, overwrite: bool) -> None:
 def download(url: str, file: str, overwrite: bool = False) -> None:
     print(f'download from {url} to {file} (overwrite={overwrite})')
     validate_not_exists(file, overwrite)
-    os.makedirs(os.path.dirname(file), exist_ok=True)
+    os.makedirs(os.path.dirname(file), exist_ok=True)  
+    if os.name == 'posix':  # check if os is linux distribution
+        _download(url, file)
+    else:
+        _downloadV2(url, file)
+
+def _download(url: str, file: str) -> None:
     os.system(f'wget -c {url} -O {file}')
+
+def _downloadV2(url: str, file: str) -> None:
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(file, 'wb') as f:
+            for chunk in tqdm(r.iter_content(chunk_size=8192), desc=f'download {file}'):
+                f.write(chunk)
 
 def extract_tar(file: str, folder: str, overwrite: bool = False) -> None:
     print(f'extract tar from {file} to {folder} (overwrite={overwrite})')
